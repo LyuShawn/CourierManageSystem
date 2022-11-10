a52e730b6b6c0d82c02335665f6f83f633e58931
LyuShawn <lvlalala@qq.com>
2022-11-10 22:37:26 +0800

    [Update]快递员端底部导航栏

--- mini-app-courier/main.js
+++ mini-app-courier/main.js
@@ -1,13 +1,19 @@
 import App from './App'
+import uView from '@/uni_modules/uview-ui'
 
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
+Vue.use(uView)
+//引入分离的api接口
+// import * as api from './api/api.js'
+//Vue.prototype.$api = api
 App.mpType = 'app'
 const app = new Vue({
     ...App
 })
 app.$mount()
+
 // #endif
 
 // #ifdef VUE3
---
+++ mini-app-courier/pages/my-outlets/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h1>这是我的网点页面</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
