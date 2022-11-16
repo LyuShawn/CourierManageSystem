e26b3867c3e28b5c558ba12a20a3be6de98db49f
LyuShawn <lvlalala@qq.com>
2022-11-16 17:18:44 +0800

    [Update]完善信息页面

--- mini-app-courier/api/api.js
+++ mini-app-courier/api/api.js
@@ -2,14 +2,37 @@ import {
 	request
 } from './request.js'
 
-export class Courier{
-	static login(code){
+export class Courier {
+	static login(code) {
 		return request({
-			url:'/courier/login',
-			method:'post',
-			data:{
+			url: '/courier/login',
+			method: 'post',
+			data: {
 				code
 			}
+		})
+	}
+	
+	static changeInfo(info){
+		return request({
+			url:'/courier/information_change',
+			method:'post',
+			data:{
+				open_id:info.open_id,
+				full_name:info.fullName,
+				phone:info.phoneNumber,
+				avatar_url:info.avatarUrl
+			}
+		})
+	}
+}
+
+export class Outlet {
+	static getList() {
+		return request({
+			url: '/admin/show_outlets',
+			method: 'get',
+			data: {}
 		})
 	}
 }
--- mini-app-courier/pages/user/my-info/index.vue
+++ mini-app-courier/pages/user/my-info/index.vue
@@ -9,11 +9,35 @@
 
 			<view class="name-view form-item">
 				<view class="form-item-title">姓名</view>
-				<view class="name-input">
-					<u--input :value="courierInfo.fullName" placeholder="请输入姓名"></u--input>
+				<view class="name-input form-item-content">
+					<u--input v-model="courierInfo.fullName" border="none" fontSize="40rpx" placeholder="请输入姓名">
+					</u--input>
 				</view>
 			</view>
+			<view class="form-item phone-view">
+				<view class="form-item-title">手机号</view>
+				<view class="form-item-content">
+					<u--input v-model="courierInfo.phoneNumber" border="none" fontSize="40rpx" placeholder="请输入手机号">
+					</u--input>
+				</view>
+			</view>
+
+			<view class="form-item outlet-view">
+				<view class="form-item-title">所属网点</view>
+				<view class="form-item-content" @click="showOutlet=true">
+					<view class="outlet-select">{{courierInfo.outlets?courierInfo.outlets.nickname:'请选择网点'}}</view>
+				</view>
+			</view>
+
+			<view class="form-item confirm-btn">
+				<u-button size="large" type="primary" hairline="false" plain text="确定" @click="submitInfo"></u-button>
+			</view>
 		</view>
+
+		<!-- 网点选择器 -->
+		<u-picker keyName="nickname" :show="showOutlet" :columns="outletList" :closeOnClickOverlay="true"
+			@cancel="showOutlet=false" @close="showOutlet=false" @confirm="confirmOutlet"></u-picker>
+		<u-toast ref="uToast"></u-toast>
 	</view>
 </template>
 
@@ -25,16 +49,71 @@
 					avatarUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132',
 					fullName: '',
 					phoneNumber: '',
-					outlets: '',
+					outlets: null,
 
 				},
+				showOutlet: false,
+				outletList: [],
+
 			};
 		},
 		methods: {
+
+			showToast(params) {
+				this.$refs.uToast.show({
+					...params,
+					complete() {
+						params.url && uni.navigateTo({
+							url: params.url
+						})
+					}
+				})
+			},
+
 			onChooseAvatar(e) {
 				this.courierInfo.avatarUrl = e.detail.avatarUrl;
+			},
+			confirmOutlet(e) {
+				this.courierInfo.outlets = e.value[0]
+				this.showOutlet = false
+			},
+			submitInfo() {
+				//校验
+				let _this = this
+				if (uni.$u.test.isEmpty(this.courierInfo.fullName)) {
+					return this.showToast({
+						message: '姓名不能为空',
+						type: 'error'
+					})
+				}
+				if (uni.$u.test.mobile(this.courierInfo.phoneNumber)) {
+					return this.showToast({
+						message: '请正确输入手机号',
+						type: 'error'
+					})
+				}
+				if (uni.$u.test.isEmpty(this.courierInfo.outlets)) {
+					return this.showToast({
+						message: '请选择网点',
+						type: 'error'
+					})
+				}
+				//手机号校验未开启
+				this.showToast({
+					message:'信息完善成功',
+					type:'success'
+				})
+				
+
 			}
 		},
+		onLoad() {
+			console.log('完善个人信息页面onload函数调用');
+			let _this = this
+			this.$api.Outlet.getList().then((res) => {
+				_this.outletList.push(res.data.data)
+			})
+		}
 	}
 </script>
 
@@ -47,7 +126,7 @@
 		align-items: center;
 
 		.form-item {
-			margin-top: 80rpx;
+			margin-top: 60rpx;
 			display: flex;
 			flex-direction: column;
 
@@ -57,12 +136,34 @@
 				margin-bottom: 16rpx;
 			}
 
-			.form-item-content {}
+			.form-item-content {
+				padding: 18rpx;
+				border-radius: 12rpx;
+				box-shadow: 0 0 20upx rgba(0, 0, 0, 0.15);
+			}
 		}
 
 		.name-view {
 			width: 70vw;
 
+		}
+
+		.phone-view {
+			width: 70vw;
+		}
+
+		.outlet-view {
+			width: 70vw;
+
+			.outlet-select {
+				font-size: 40rpx;
+				color: #777;
+			}
+		}
+
+		.confirm-btn {
+			width: 200rpx;
+			margin-top: 120rpx;
 		}
 
 		.avatar-view {
